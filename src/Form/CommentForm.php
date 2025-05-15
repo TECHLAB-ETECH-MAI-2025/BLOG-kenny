c858cd70cc00245cd4a5fde9fc25853f5c068007
kenny516 <kennyandriantsirafychan@gmail.com>
2025-05-15 14:19:14 +0300

    add comment crud and updationg styling and refactor

---
+++ src/Form/CommentForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Comment;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Écrivez votre commentaire ici',
+                    'rows' => 5
+                ]
+            ])
+            ->add('article', EntityType::class, [
+                'class' => Article::class,
+                'label' => 'Article',
+                'choice_label' => 'title',
+                'placeholder' => 'Choisissez un article...',
+                'required' => true
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
