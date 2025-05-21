9539c08ae7d6c520cc3024abec1abc54240fd637
kenny516 <kennyandriantsirafychan@gmail.com>
2025-05-21 16:17:52 +0300

    Refactor article form and list UI with improved styling and validation

--- src/Form/ArticleForm.php
+++ src/Form/ArticleForm.php
@@ -6,27 +6,47 @@ use App\Entity\Article;
 use App\Entity\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ArticleForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', null, [
+            ->add('title', TextType::class, [
                 'label' => 'Titre',
                 'attr' => [
-                    'class' => 'form-control',
-                    'placeholder' => 'Entrez le titre de l\'article'
+                    'placeholder' => 'Entrez le titre de l\'article',
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Le titre ne peut pas être vide']),
+                    new Length([
+                        'min' => 3,
+                        'max' => 255,
+                        'minMessage' => 'Le titre doit contenir au moins {{ limit }} caractères',
+                        'maxMessage' => 'Le titre ne peut pas dépasser {{ limit }} caractères'
+                    ])
                 ]
             ])
-            ->add('content', null, [
+            ->add('content', TextareaType::class, [
                 'label' => 'Contenu',
                 'attr' => [
-                    'class' => 'form-control',
                     'placeholder' => 'Rédigez votre article ici',
-                    'rows' => 10
+                    'rows' => 10,
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Le contenu ne peut pas être vide']),
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => 'Le contenu doit contenir au moins {{ limit }} caractères'
+                    ])
                 ]
             ])
             ->add('categories', EntityType::class, [
@@ -36,11 +56,9 @@ class ArticleForm extends AbstractType
                 'expanded' => false,
                 'label' => 'Catégories',
                 'attr' => [
-                    'class' => 'form-select select2',
-                    'data-placeholder' => 'Sélectionnez les catégories...',
-                    'multiple' => 'multiple'
+                    'class' => 'form-control'
                 ],
-                'by_reference' => false,
+                'by_reference' => false
             ])
         ;
     }
